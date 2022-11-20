32a9e701d9afa38aa0c7feef567bfb3e683328f7
kavulox <108770990+kavulox@users.noreply.github.com>
2022-11-19 22:43:25 -0500

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -76,7 +76,7 @@ static const char *termcmd[]  = { "kitty", NULL };
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show drun") },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
